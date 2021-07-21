542d5edf3aba07965e4013bb0f07077e7d2b15ff
Deo <lukyanenko88@gmail.com>
2021-07-21 21:00:31 +0300

    add default  error details messages for not null id field and data setup in past time

--- src/main/java/com/deo/todoer/exception/CustomizedResponseEntityExceptionalHandler.java
+++ src/main/java/com/deo/todoer/exception/CustomizedResponseEntityExceptionalHandler.java
@@ -4,12 +4,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
-
+@ControllerAdvice
+@RestController
 public class CustomizedResponseEntityExceptionalHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
--- src/main/java/com/deo/todoer/todo/ToDo.java
+++ src/main/java/com/deo/todoer/todo/ToDo.java
@@ -1,20 +1,21 @@
 package com.deo.todoer.todo;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.Future;
+import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 import java.util.Date;
 @AllArgsConstructor
 @Getter
 @Setter
 public class ToDo {
+    @Null(message = "The id will be set automatically")
     private Integer id;
     @Size(min = 1)
     private String name;
-    @Future
+    @Future(message = "You cant choose the date in past")
     private Date date;
 }
